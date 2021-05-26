edeeebe42d6696b4e3b3c8ea664a23e6bf351fd6
2xP <theonlystrongman@gmail.com>
2021-05-26 21:34:55 +0200

    database connection fix

--- corporation-service/src/main/java/corporation/service/EmployeeService.java
+++ corporation-service/src/main/java/corporation/service/EmployeeService.java
@@ -3,9 +3,11 @@ package corporation.service;
 import corporation.repository.Employee;
 import corporation.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
